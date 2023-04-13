95bf9d0da3e4eb71be314c199dda0cdb9f2e09bb
younousse houssene <younoussehoussene@gmail.com>
2023-04-13 13:31:14 +0200

    correction rythme carrousel

--- assets/script.js
+++ assets/script.js
@@ -25,84 +25,85 @@ const precedent = document.querySelector(' .arrow_left')
 const slideImg = document.querySelector('.banner-img')
 const TextImg = document.querySelector('#banner > p');
 
-console.log(suivant)
 
 
 
 
- 
+
+
 
 /* function prev / next */
 
 function slideSuivant() {
 	num = num + 1;
-	if (num > slides.length - 1){
+	if (num > slides.length - 1) {
 		num = 0;
 	}
 	slideImg.src = "./assets/images/slideshow/" + slides[num].image;
 	TextImg.innerHTML = slides[num].tagLine;
-	
+
 }
 
 suivant.addEventListener('click', slideSuivant)
 
 function slideprecedent() {
 	num = num - 1;
-	if (num < 0){
-		num = slides.length - 1; 
-		
+	if (num < 0) {
+		num = slides.length - 1;
+
 	}
 	slideImg.src = "./assets/images/slideshow/" + slides[num].image;
 	TextImg.innerHTML = slides[num].tagLine;
-	
+
 
 }
 
 precedent.addEventListener('click', slideprecedent)
 
 
-/* Creation bullet (dots) */
+/* Creation dots */
 
 
-function createDot(){
-	
+function createDot() {
+
 	const dots = document.querySelector('.dots');
-	
-	for ( let i = 0; i < slides.length ; i++){
+
+	for (let j = 0; j < slides.length; j++) {
 		const dot = document.createElement('div');
 		dot.classList.add('dot');
 		dots.appendChild(dot);
 
-		if (i == 0) { 
-			dots.children[i].classList.add('dot_selected');
+		if (j === 0) {
+			dots.children[j].classList.add('dot_selected');
 		}
-		}
-
-		
 	}
-	
+
+
+}
 
 createDot();
-console.log(createDots)
+
+
+console.log(createDot)
+
+
 
 
 /* Link bullet to image (dot) */
 
-
 function dotSwitch() {
-	const dot = document.querySelector('dot');
+	const dot = document.querySelector('.dot');
 
 	for (let i = 0; i < dot.length; i++) {
 		dot[i].classList.remove('dot_selected');
-		dot[i].classList.add('dot_selected');
+		dot[i].classList.add('dot');
 	}
- }
- dotSwitch();
+}
+dotSwitch();
 
-console.log(createDot)
+console.log(dotSwitch)
 
-setInterval('slideSuivant(1)',6000
-)
+setInterval('slideprecedent(1)', 5000)
 
 
 
